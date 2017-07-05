3e8fdb1d8402f844c88d0bb48cc189f042413318
BalakinVladislav <balakinv95@gmail.com>
2017-07-05 22:31:46 +0500

    try1

--- index.js
+++ index.js
@@ -8,4 +8,5 @@ $('#button2').on('click', function() {
 
 $('#button3').on('click', function() {
   ga('send', 'event', 'Button3', 'Действия', 'Иллюстрация', 'Ценность3');
+  _gaq.push(['_trackEvent', 'Категория', 'Действия', 'Ярлык', 'Ценность'];
 });
