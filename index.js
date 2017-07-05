4de2bfc91b02f9a7e562b4367604f00fed0070ba
BalakinVladislav <balakinv95@gmail.com>
2017-07-06 03:21:28 +0500

    try6

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+$(function(){
 $('#button1').on('click', function() {
 	console.log('2 обработчик события click');
 	ga('send', {
@@ -5,20 +6,26 @@ $('#button1').on('click', function() {
   eventCategory: 'Button1',
   eventAction: 'Нажатие',
   eventLabel: 'Ценность1'
-})});
-
+})
+});
+});
+$(function(){
 $('#button2').on('click', function() {
   ga('send', {
   hitType: 'event',
   eventCategory: 'Button2',
   eventAction: 'Нажатие',
   eventLabel: 'Ценность2'
-})});
-
+})
+});
+});
+$(function(){
 $('#button3').on('click', function() {
   ga('send', {
   hitType: 'event',
   eventCategory: 'Button3',
   eventAction: 'Нажатие',
   eventLabel: 'Ценность3'
-})});
+})
+});
+});
