dac93d3cae6e34191151243748537c608c6b3818
BalakinVladislav <balakinv95@gmail.com>
2017-07-05 22:25:13 +0500

    add buttons to GA

---
+++ index.js
@@ -0,0 +1,11 @@
+$('#button1').on('click', function() {
+  ga('send', 'event', 'Button1', 'Действия', 'Иллюстрация', 'Ценность1');
+});
+
+$('#button2').on('click', function() {
+  ga('send', 'event', 'Button2', 'Действия', 'Иллюстрация', 'Ценность2');
+});
+
+$('#button3').on('click', function() {
+  ga('send', 'event', 'Button3', 'Действия', 'Иллюстрация', 'Ценность3');
+});
